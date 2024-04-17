6a3af5fd0e57dba0fa8bde92220c79364c32cbfd
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-17 23:25:43 +0800

    add cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,18 @@
 import { cli } from 'cleye'
 import { confirm, text, intro, outro} from '@clack/prompts';
+import os from 'node:os';
+
+const cups = os.cpus().length;
+console.log(`你的电脑有${cups}个核心`)
+
 
 // Parse argv
 const argv = cli({
 	name: 'cli.ts',
 
 	// Define parameters
-	parameters: [
-		'<first name>', // First name is required
-		'[last name]' // Last name is optional
-	],
+	// parameters: [
+	// ],
 
 	// 里面定义--options 参数
 	flags: {  
@@ -17,25 +20,5 @@ const argv = cli({
 	}
 })
 
-intro(`Hello👋, 找我有什么事吗`);
-
-const shouldContinue = await confirm({
-	message: '张庆风你又摸鱼了，怎么了，有什么想说的吗?',
-});
-
-const meaning = await text({
-	message: '那你说吧，我听着～',
-	placeholder: 'Not sure',
-	initialValue: '张庆风和杨诗颖要一起加油呀',
-	validate(value) {
-		if (value.length === 0) return `Value is required!`;
-	},
-});
-const print = [argv._.firstName, argv._.lastName].filter(Boolean).join('和')
-const loveStr = print + ' 你们要一起好好加油啊！祝福你们🎆'
-
-if (meaning === '张庆风和杨诗颖要一起加油呀') {
-	outro(loveStr);	
-}
 
 
