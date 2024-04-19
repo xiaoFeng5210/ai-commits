6b838cd70f461f1e132dbfa8016efc0fc2c04b60
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-19 16:00:33 +0800

    测试openai key

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,9 +1,38 @@
-import {https} from "https"
+import https from "https"
 import type { ClientRequest, IncomingMessage } from 'http';
 import type {
 	CreateChatCompletionRequest,
 	CreateChatCompletionResponse,
 } from 'openai';
+import * as process from "node:process";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-console.log('api key')
+const API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+const callOpenAI = async () => {
+  	new Promise((resolve, reject) => {
+			https.request({
+				port: 443,
+				host: 'api.openai.com',
+				path: '/v1/chat/completions',
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${OPENAI_API_KEY}`,
+					'Content-Type': 'application/json',
+				}
+			})
+	  })
+}
+
+const createChatCompletion = async () => {
+	console.log(OPENAI_API_KEY);
+	if (!OPENAI_API_KEY) {
+		throw new Error('No OpenAI API key found');
+	}
+	
+	
+	await callOpenAI()
+	
+}
+
+await createChatCompletion()
