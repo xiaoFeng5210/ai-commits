55e510ec3a428bdba1dffef1afc1505fdbecd0ca
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-19 17:59:50 +0800

    调通接口

--- src/utils/help.ts
+++ src/utils/help.ts
@@ -1,5 +1,9 @@
 import {execSync} from "node:child_process";
+import fs from "node:fs";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
 
+const __dirname = fileURLToPath(import.meta.url);
 /**
  * Check if a file has changed in the git index
  * @param path
@@ -7,4 +11,10 @@ import {execSync} from "node:child_process";
 export function getFilesChangedInGitAdd() {
 	const gitDiff = execSync("git diff --cached --name-only", { encoding: "utf-8" });
 	return gitDiff.split("\n")
+}
+
+export function getOpenAIkey() {
+	const filePath = path.join(__dirname, "..", "..", "..", ".env")
+	const content = fs.readFileSync(filePath, { encoding: "utf-8" });
+	return content.split("=")[1].replace("\n", "")
 }
--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -4,35 +4,72 @@ import type {
 	CreateChatCompletionRequest,
 	CreateChatCompletionResponse,
 } from 'openai';
-import * as process from "node:process";
+import {getOpenAIkey} from "./help";
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+let OPENAI_API_KEY;
 
-const callOpenAI = async () => {
+const callOpenAI = async (json: CreateChatCompletionRequest) => {
   	new Promise((resolve, reject) => {
-			https.request({
+			const postBody = JSON.stringify(json)
+			const request = https.request({
 				port: 443,
 				host: 'api.openai.com',
 				path: '/v1/chat/completions',
 				method: 'POST',
+				timeout: 20000,
 				headers: {
 					Authorization: `Bearer ${OPENAI_API_KEY}`,
 					'Content-Type': 'application/json',
 				}
-			})
+			},
+			response => {
+				const res: Buffer[] = []
+				response.on('data', chunk => res.push(chunk))
+				response.on('end', () => {
+					console.log(Buffer.concat(res).toString())
+					resolve({
+						request,
+						data: Buffer.concat(res).toString()
+					})
+				})
+			}
+			)
+		  
+		  request.on('error', reject)
+		  request.on('timeout', () => {
+			  request.destroy();
+				reject(new Error(`Request timeout`))
+		  })
+		  request.write(postBody)
+		  request.end()
 	  })
 }
 
 const createChatCompletion = async () => {
-	console.log(OPENAI_API_KEY);
+	OPENAI_API_KEY = getOpenAIkey()
+	console.log(OPENAI_API_KEY)
 	if (!OPENAI_API_KEY) {
 		throw new Error('No OpenAI API key found');
 	}
+	// TODO: prompt 信息先随机一个
+	const json: CreateChatCompletionRequest = {
+		model: 'gpt-3.5-turbo',
+		// model: 'gpt-4-1106-preview',
+		messages: [
+			{ role: 'system', content: 'You are a helpful assistant.' },
+			{ role: 'user', content: '我想知道幸福的生活是什么样的' },
+		],
+		temperature: 0.7,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+		max_tokens: 200,
+		stream: false,
+		// 建议的答案数量
+		n: 1,
+	};
 	
-	
-	await callOpenAI()
-	
+	const res = await callOpenAI(json)
 }
 
-await createChatCompletion()
+createChatCompletion()
